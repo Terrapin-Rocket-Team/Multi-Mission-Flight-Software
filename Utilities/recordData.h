c99ba3a21c0a37fb08ec9aca58e34d042ba229b1
Michael Mallamaci <mallamacimj@gmail.com>
2023-07-16 17:58:17 -0700

    got rid of CSV_HEADER

--- Utilities/recordData.h
+++ Utilities/recordData.h
@@ -11,8 +11,6 @@ int PRE_FLIGHT_DATA_DUMP_DURATION = 600;  //in seconds
 int PRE_FLIGHT_TIME_SINCE_LAST_DUMP = 0;  //in seconds
 int PRE_FLIGHT_TIME_OF_LAST_DUMP = 0;  //in seconds
 
-String CSV_HEADER;
-
 void recordData(State& state, String stage);  //Stages can be "PreFlight", "Flight", "PostFlight"
 void dataToPSRAM(State& state);
 
