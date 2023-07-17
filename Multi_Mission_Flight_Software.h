30bf17756ac49bcecfcf8997c2bc79a654a9e52f
mallamacimj <64282214+mallamacimj@users.noreply.github.com>
2023-07-16 20:14:34 -0400

    Create Multi_Mission_Flight_Software.h

---
+++ Multi_Mission_Flight_Software.h
@@ -0,0 +1,18 @@
+#ifndef MULTI_MISSION_FLIGHT_SOFTWARE_H
+#define MULTI_MISSION_FLIGHT_SOFTWARE_H
+
+//Include all the Sensors
+#include "Barometer.h"
+#include "GPS.h"
+#include "IMU.h"
+#include "LightSensor.h"
+
+//Include State
+#include "State.h"
+
+//Include all the Utilities
+#include "buzzer.h"
+#include "recordData.h"
+#include "time.h"
+
+#define
