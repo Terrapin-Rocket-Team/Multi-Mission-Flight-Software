9e2ae63217c404c66357d4d42b8bba8a4e19892e
ChenJeff123 <143658756+ChenJeff123@users.noreply.github.com>
2024-01-12 22:39:26 -0500

    shifted pinMode out of the loop and added a one second delay

--- Utilities/buzzer.cpp
+++ Utilities/buzzer.cpp
@@ -1,10 +1,11 @@
 #include "buzzer.h"
 
 void buzz(int buzzer_pin, int durationOfBuzz, int counter){
+    pinMode(buzzer_pin, OUTPUT);
     for (int i = 0; i < counter; i++) {
-        pinMode(buzzer_pin, OUTPUT);
         digitalWrite(buzzer_pin, HIGH);
         delay(durationOfBuzz);
         digitalWrite(buzzer_pin, LOW);
+        delay(1000);
     }
 }
