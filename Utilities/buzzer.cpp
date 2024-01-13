7d40c4ac84af8575c7176056603b6e40630c846e
ChenJeff123 <143658756+ChenJeff123@users.noreply.github.com>
2024-01-12 21:27:35 -0500

    added a counter functionality to allow for multiple buzzes

--- Utilities/buzzer.cpp
+++ Utilities/buzzer.cpp
@@ -1,8 +1,10 @@
 #include "buzzer.h"
 
-void buzz(int buzzer_pin, int durationOfBuzz){
-    pinMode(buzzer_pin, OUTPUT);
-    digitalWrite(buzzer_pin, HIGH);
-    delay(durationOfBuzz);
-    digitalWrite(buzzer_pin, LOW);
+void buzz(int buzzer_pin, int durationOfBuzz, int counter){
+    for (int i = 0; i < counter; i++) {
+        pinMode(buzzer_pin, OUTPUT);
+        digitalWrite(buzzer_pin, HIGH);
+        delay(durationOfBuzz);
+        digitalWrite(buzzer_pin, LOW);
+    }
 }
