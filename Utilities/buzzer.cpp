63cc1058cf423c1e3ec2b65e1a5acab839ac3008
Michael Mallamaci <mallamacimj@gmail.com>
2024-02-24 12:54:32 -0500

    added extra buzzer 2 input arg

--- Utilities/buzzer.cpp
+++ Utilities/buzzer.cpp
@@ -1,5 +1,12 @@
 #include "buzzer.h"
 
+void buzz(int buzzer_pin, int durationOfBuzz){
+    pinMode(buzzer_pin, OUTPUT);
+    digitalWrite(buzzer_pin, HIGH);
+    delay(durationOfBuzz);
+    digitalWrite(buzzer_pin, LOW);
+}
+
 void buzz(int buzzer_pin, int durationOfBuzz, int counter){
     pinMode(buzzer_pin, OUTPUT);
     for (int i = 0; i < counter; i++) {
