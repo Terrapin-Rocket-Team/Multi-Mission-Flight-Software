272fcd72ae9f92c7b9abebb971521ce5cf3a1e68
Drew Brandt <ahbrandt@umd.edu>
2024-10-22 14:43:54 -0400

    fixed test failures

--- test/test_logger/TestingLogger.h
+++ test/test_logger/TestingLogger.h
@@ -8,7 +8,7 @@ using namespace mmfs;
 class TestingLogger : public Logger
 {
     public:
-        TestingLogger(uint16_t bufferTime = 30, int bufferInterval = 30, bool packData = true, GroundMode mode = ALTERNATE_) : Logger(bufferTime, bufferInterval, packData, mode) {};
+        TestingLogger(uint16_t bufferTime = 30, int bufferInterval = 30, bool packData = true, GroundMode mode = ALTERNATE_) : Logger(bufferTime, bufferInterval) {};
         virtual ~TestingLogger() {};
 
         SdFs &getSdFs() { return sd; };
