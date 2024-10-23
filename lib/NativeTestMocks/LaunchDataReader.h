54f75c38f0f9dc8e6a5e97b77ad4db2ee4b6910f
Ramy Kaddouri <myramkadd@gmail.com>
2024-10-22 20:38:20 -0400

    fix build issue

--- lib/NativeTestMocks/LaunchDataReader.h
+++ lib/NativeTestMocks/LaunchDataReader.h
@@ -5,7 +5,7 @@
 #ifndef LAUNCHDATAREADER_H
 #define LAUNCHDATAREADER_H
 #include <filesystem>
-#include <string>
+#include <fstream>
 
 
 class LaunchDataReader {
@@ -14,7 +14,7 @@ private:
     size_t lineIdx;
     const std::filesystem::path& filePath;  //fs::path is cross-platform,
                                             //should be easier to work with
-    std::ifstream<char> fileStream;
+    std::ifstream fileStream;
     size_t numColumns;
 public:
     LaunchDataReader(const std::filesystem::path& filePath);
