c128fa91496e0420903de635b6d197036db1a8f8
Ramy Kaddouri <myramkadd@gmail.com>
2024-10-24 21:52:41 -0400

    quick test, read header seems to work

--- lib/NativeTestMocks/LaunchDataReader.cpp
+++ lib/NativeTestMocks/LaunchDataReader.cpp
@@ -4,14 +4,58 @@
 
 #include "LaunchDataReader.h"
 
+#include <cstring>
 #include <iostream>
 
 LaunchDataReader::LaunchDataReader(const std::filesystem::path &filePath) : filePath(filePath), fileStream(filePath) {
     if(!fileStream.is_open()) {
         std::cerr << "LaunchDataReader: Failed to open data file at " << filePath << std::endl;
         initialized = false;
+    } else {
+        initialized = true;
     }
 }
+
+bool LaunchDataReader::read_column_header(int &numCols, char **colNames) {
+    if(lineIdx == 0) {
+        std::string line;
+        std::getline(fileStream, line);
+        std::istringstream headerStream = std::istringstream(line);
+
+        std::string col;
+        numCols = 0;
+        while(std::getline(headerStream, col, ',')) {
+            strcpy(colNames[numCols], col.c_str());
+            numCols++;
+        }
+
+        lineIdx++;
+        return true;
+    } else {
+        std::cerr << "LaunchDataReader::read_column_header: Line index is not 0. This function must be called before any reads!" << std::endl;
+        return false;
+    }
+}
+
+bool LaunchDataReader::read(float *data) {
+    std::string line;
+    std::getline(fileStream, line);
+    std::istringstream lineStream = std::istringstream(line);
+
+    std::string col;
+    size_t i = 0;
+    while(lineStream.good()) {
+        float measurement;
+        lineStream >> measurement;
+        data[i] = measurement;
+        i++;
+    }
+
+    lineIdx++;
+
+    return true;
+}
+
 
 bool LaunchDataReader::is_initialized() const {
     return initialized;
---
+++ test/test_launch_data_reader/TestLaunchDataReader.cpp
@@ -0,0 +1,68 @@
+#include <unity.h>
+#include <iostream>
+#include "../../lib/NativeTestMocks/NativeTestHelper.h"
+
+// include other headers you need to test here
+
+#include "../../lib/NativeTestMocks/LaunchDataReader.h"
+
+// ---
+
+
+
+// Set up and global variables or mocks for testing here
+
+
+
+// ---
+
+
+
+// These two functions are called before and after each test function, and are required in unity, even if empty.
+void setUp(void)
+{
+    // set stuff up before each test here, if needed
+}
+
+void tearDown(void)
+{
+    // clean stuff up after each test here, if needed
+}
+// ---
+
+
+
+// Test functions must be void and take no arguments, put them here
+
+void test_read_header() {
+     LaunchDataReader reader("./test/test_launch_data_reader/test.csv");
+     char *cols[10];
+     for(int i = 0; i < 10; i++) {
+       cols[i] = (char*) malloc(1024 * sizeof(char));
+     }
+
+     int numCols;
+     bool res = reader.read_column_header(numCols, cols);
+//     TEST_ASSERT_EQUAL(true, reader.is_initialized());
+
+//     std::cout << "Number of columns: " << numCols << std::endl;
+//     std::cout << "col 1" << cols[0] << std::endl;
+     TEST_ASSERT_EQUAL(0, strcmp(cols[0], "col1"));
+}
+
+// ---
+
+
+
+// This is the main function that runs all the tests. It should be the last thing in the file.
+int main(int argc, char **argv)
+{
+    UNITY_BEGIN();
+
+    // Add your tests here
+    RUN_TEST(test_read_header); // no parentheses after function name
+
+
+    UNITY_END();
+}
+// ---
