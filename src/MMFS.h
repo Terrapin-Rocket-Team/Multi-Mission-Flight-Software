b61b89c1bfe1ac12aba9002f11dbacb13fb41d44
Michael Mallamaci <mallamacimj@gmail.com>
2024-11-20 20:53:14 -0500

    Update MMFS.h

--- src/MMFS.h
+++ src/MMFS.h
@@ -8,7 +8,7 @@
 #include "Sensors/GPS/MAX_M10S.h"
 #include "Sensors/LightSensor/LightSensor.h"
 #include "Sensors/Baro/BMP280.h"
-#include "Sensors/Encoder/Encoder.h"
+#include "Sensors/Encoder/Encoder_MMFS.h"
 #include "State/State.h"
 #include "Constants.h"
 #include "Filters/LinearKalmanFilter.h"
