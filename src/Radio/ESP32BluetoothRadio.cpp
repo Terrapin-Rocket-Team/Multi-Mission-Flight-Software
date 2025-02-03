15c35eec291c74c26b016bc7ed7b0a915c86eb4d
Ramy Kaddouri <myramkadd@gmail.com>
2025-02-03 16:57:26 -0500

    exclude radio stuff from native build

--- src/Radio/ESP32BluetoothRadio.cpp
+++ src/Radio/ESP32BluetoothRadio.cpp
@@ -13,24 +13,30 @@ mmfs::ESP32BluetoothRadio::~ESP32BluetoothRadio() {
 
 bool mmfs::ESP32BluetoothRadio::begin() {
     port.begin(9600);
+    return false;
 }
 
 bool mmfs::ESP32BluetoothRadio::tx(const uint8_t *message, int len) {
     port.write(message, len);
+    return false;
 }
 
 bool mmfs::ESP32BluetoothRadio::rx() {
     // uint8_t buf[1024];
     // port.readBytes(buf, 1024);
+    return false;
 }
 
 bool mmfs::ESP32BluetoothRadio::send(Data &data) {
     Message message;
     data.encode(message.buf, message.size);
+    return false;
 }
 
 bool mmfs::ESP32BluetoothRadio::receive(Data &data) {
+    return false;
 }
 
 int mmfs::ESP32BluetoothRadio::RSSI() {
+    return 0;
 }
