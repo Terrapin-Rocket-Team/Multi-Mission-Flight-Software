5533000ced46c9623659ae417e9090bfa83ef406
Michael Mallamaci <mallamacimj@gmail.com>
2025-02-26 11:32:52 -0500

    mockbaro using mmfs namespace

--- src/Sensors/Baro/MockBarometer.cpp
+++ src/Sensors/Baro/MockBarometer.cpp
@@ -5,8 +5,8 @@
 
 #include "MockBarometer.h"
 
-#include <iostream>
-
+using namespace mmfs
+{
 MockBarometer::MockBarometer(const char* &dataPath, const String& pressureColName, const String& temperatureColName) :
     dataReader(dataPath), pressureColName(pressureColName), temperatureColName(temperatureColName) {
     Sensor::setName("MockBarometer");
@@ -65,4 +65,5 @@ void MockBarometer::read() {
 
 MockBarometer::~MockBarometer() {
     dataReader.close();
+}
 }
