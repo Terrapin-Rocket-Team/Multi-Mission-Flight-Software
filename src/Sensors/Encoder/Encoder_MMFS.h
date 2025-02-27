399f5ee1e16095822257a0322de2c05de8664a5a
Michael Mallamaci <mallamacimj@gmail.com>
2025-02-27 17:40:25 -0500

    created a set encoder steps method

--- src/Sensors/Encoder/Encoder_MMFS.h
+++ src/Sensors/Encoder/Encoder_MMFS.h
@@ -15,6 +15,7 @@ namespace mmfs
         virtual const SensorType getType() const override;
         virtual void update() override;
         virtual bool begin(bool useBiasCorrection = true) override;
+        virtual void setCurrentRelativeSteps(int step) {currentRelativeSteps = step;};
 
     protected:
         Encoder_MMFS();
