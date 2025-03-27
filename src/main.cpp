2fab27c83ad4ec9f797fcf64a345b6122aab41f0
Drew Brandt <ahbrandt@umd.edu>
2025-03-26 20:36:57 -0400

    removed testing main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,32 @@
-//
-// Created by ramykaddouri on 3/24/25.
-//
+// //
+// // Created by ramykaddouri on 3/24/25.
+// //
 
-#include <Arduino.h>
-#include <Radio/ESP32BluetoothRadio.h>
-#include <MMFS.h>
+// #include <Arduino.h>
+// #include <Radio/ESP32BluetoothRadio.h>
+// #include <MMFS.h>
 
 
-#define BTSERIAL Serial1
-mmfs::ESP32BluetoothRadio btRadio(BTSERIAL, "TRT_BT");
+// #define BTSERIAL Serial1
+// mmfs::ESP32BluetoothRadio btRadio(BTSERIAL, "TRT_BT");
 
-void setup() {
-    Serial.begin(9600);
-    Serial.println("Hello World!");
-    btRadio.begin();
+// void setup() {
+//     Serial.begin(9600);
+//     Serial.println("Hello World!");
+//     btRadio.begin();
 
-    while (!btRadio.isReady()) {
-        delay(500);
-        btRadio.rx();
-        Serial.print(".");
-    }
-    Serial.println("");
+//     while (!btRadio.isReady()) {
+//         delay(500);
+//         btRadio.rx();
+//         Serial.print(".");
+//     }
+//     Serial.println("");
 
-    Serial.println("Sending message...");
-    const uint8_t msg[] = "Hello World!";
-    btRadio.tx(msg, sizeof(msg));
-}
+//     Serial.println("Sending message...");
+//     const uint8_t msg[] = "Hello World!";
+//     btRadio.tx(msg, sizeof(msg));
+// }
 
-void loop() {
-    btRadio.rx();
-}
+// void loop() {
+//     btRadio.rx();
+// }
