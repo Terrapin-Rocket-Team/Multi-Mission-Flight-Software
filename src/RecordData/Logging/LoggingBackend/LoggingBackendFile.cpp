8993fe298735c93eacce15277abbc8c15425e72e
Drew Brandt <ahbrandt@umd.edu>
2025-09-01 20:24:33 -0400

    updated to Print

--- src/RecordData/Logging/LoggingBackend/LoggingBackendFile.cpp
+++ src/RecordData/Logging/LoggingBackend/LoggingBackendFile.cpp
@@ -8,24 +8,9 @@ LoggingBackendFile::LoggingBackendFile(LoggingBackend *b, int fileNum)
 {
 }
 
-size_t LoggingBackendFile::write(const uint8_t *data, size_t len)
+size_t LoggingBackendFile::write(uint8_t byte)
 {
-    return b->write(fileNum, data, len);
-}
-
-size_t LoggingBackendFile::write(const char *data, size_t len)
-{
-    // If len is -1, treat it as null-terminated string
-    if (len == static_cast<size_t>(-1))
-    {
-        len = strlen(data);
-    }
-    return write((uint8_t *)data, len);
-}
-
-size_t LoggingBackendFile::write(const char data)
-{
-    return write(&data, 1);
+    return b->write(fileNum, &byte, 1);
 }
 
 void LoggingBackendFile::close()
@@ -48,11 +33,6 @@ size_t LoggingBackendFile::println(const char *data)
     size_t written = print(data);
     written += write('\n');
     return written;
-}
-
-size_t LoggingBackendFile::print(const char *data)
-{
-    return write(data);
 }
 void LoggingBackendFile::seek(long pos){
     b->seek(fileNum, pos);
--- src/RecordData/Logging/LoggingBackend/LoggingBackendFile.h
+++ src/RecordData/Logging/LoggingBackend/LoggingBackendFile.h
@@ -3,17 +3,16 @@
 
 #include <cstdint>
 #include <cstddef>
+#include <Print.h>
 namespace mmfs
 {
     class LoggingBackend;
-    class LoggingBackendFile
+    class LoggingBackendFile : public Print
     {
 
     public:
         LoggingBackendFile(LoggingBackend *b, int fileNum);
-        virtual size_t write(const uint8_t *data, size_t len);
-        virtual size_t write(const char *data, size_t len = -1);
-        virtual size_t write(const char data);
+        virtual size_t write(uint8_t byte) override;
         virtual void close();
         virtual void save();
         virtual int readBytes(char *dest, size_t len);
